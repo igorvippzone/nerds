993e3c3303f1e8974896b94e1ad3c53198ad7209
Агаларов Игорь <igorvippzone@gmail.com>
2021-06-01 11:37:17 +0300

    доделал

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function update(event) {
+  for (let dot; (dot = document.querySelector("dot")); ) {
+    dot.remove();
+  }
+  for (let i = 0; i < event.touches.length; i++) {
+    let { pageX, pageY } = event.touches[i];
+    let dot = document.createElement("dot");
+    dot.style.left = pageX - 50 + "px";
+    dot.style.top = pageY - 50 + "px";
+    document.body.appendChild(dot);
+  }
+}
+window.addEventListener("touchstart", update);
+window.addEventListener("touchmove", update);
+window.addEventListener("touchend", update);
