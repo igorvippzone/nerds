6dc99b60940add051da075c5f9db085eea9646fb
Агаларов Игорь <igorvippzone@gmail.com>
2021-06-01 11:57:58 +0300

    open

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-function update(event) {
-  for (let dot; (dot = document.querySelector("dot")); ) {
-    dot.remove();
-  }
-  for (let i = 0; i < event.touches.length; i++) {
-    let { pageX, pageY } = event.touches[i];
-    let dot = document.createElement("dot");
-    dot.style.left = pageX - 50 + "px";
-    dot.style.top = pageY - 50 + "px";
-    document.body.appendChild(dot);
-  }
-}
-window.addEventListener("touchstart", update);
-window.addEventListener("touchmove", update);
-window.addEventListener("touchend", update);
+
+
+const openBtn = document.querySelector('.contacts__button')
+const popup = document.querySelector('.popup')
+console.log(openBtn)
+openBtn.addEventListener("click", ()=>{
+  popup.classList.remove("hidden")
+})
