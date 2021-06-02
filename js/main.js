c2fbb93f3053575d971eb339def5d04f51bec232
Агаларов Игорь <igorvippzone@gmail.com>
2021-06-02 12:59:17 +0300

    popup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+const openBtn = document.querySelector(".contacts__button");
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".write__close");
+const body = document.querySelector("body");
 
+openBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  popup.classList.add("visible");
 
-const openBtn = document.querySelector('.contacts__button')
-const popup = document.querySelector('.popup')
-console.log(openBtn)
-openBtn.addEventListener("click", ()=>{
-  popup.classList.remove("hidden")
-})
+  console.log(e);
+  openBtn.setAttribute("disabled", "disabled");
+  openBtn.style.cursor = "default";
+});
+close.addEventListener("click", () => {
+  popup.classList.remove("visible");
+});
+
+body.addEventListener("click", (e) => {
+  console.log(e);
+  const active = document.querySelector(".popup.visible");
+  if (active && e.target !== active && !active.contains(e.target)) {
+    popup.classList.remove("visible");
+    openBtn.removeAttribute("disabled", "disabled");
+    openBtn.style.cursor = "pointer";
+  }
+});
